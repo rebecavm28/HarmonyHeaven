2c89c73be693a16e121c2e67c910595a8a8b1186
rebecavm28 <rebe14@hotmail.es>
2024-01-15 12:25:52 +0100

    cambios

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+function reproducirAudio() {
+    // Crear un elemento de audio
+    const audio = new Audio('C:\Users\Administrator\desktop\proyectomusica\harmonyheaven\public\assets\audio\nota LA.mp3');  // Reemplaza 'ruta/al/tu/archivo-de-audio.mp3' con la ruta de tu archivo de audio
+
+    // Reproducir el audio
+    audio.play();
+}
